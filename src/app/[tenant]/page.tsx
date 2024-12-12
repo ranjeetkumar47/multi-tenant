057f22776c8babe3d142df032935be9ba89edb7b
ranjeet <rksingh6375@gmail.com>
2024-12-12 17:40:26 +0530

    ss

--- src/app/[tenant]/page.tsx
+++ src/app/[tenant]/page.tsx
@@ -1,22 +1,16 @@
-import { Metadata } from "next";
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Tenant Landing Page",
-};
-
-interface Props {
-  params: { tenant: string };
+  title: 'Tenant Landing Page'
 }
 
-const TenantLandingPage = ({ params }: Props) => {
-  const { tenant } = params;
-
+const TenantLandingPage = () => {
   return (
     <div className="container mx-auto text-center">
-      <h1 className="text-4xl font-bold">Welcome to {tenant}</h1>
-      <p className="text-lg mt-4">This is the landing page for {tenant}.</p>
+      <h1 className="text-4xl font-bold">Welcome to teneat</h1>
+      <p className="text-lg mt-4">This is the landing page for tejhdhjsgdjh.</p>
     </div>
-  );
-};
+  )
+}
 
-export default TenantLandingPage;
+export default TenantLandingPage
